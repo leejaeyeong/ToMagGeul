955619b9a12a0b4d099880c9613b2147370098da
sa11k <sdk4590@gmail.com>
2020-09-10 23:59:13 +0900

    토막글-글모음

--- ToMagGeul/models.py
+++ ToMagGeul/models.py
@@ -1,5 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from genre.models import Genre
 
-# 주석!
+from user.models import TMAuthor
+
+class TMseries(models.Model):
+    series_id = models.AutoField(primary_key=True)
+    series_title = models.CharField(max_length=30)
+    introduce = models.CharField(max_length=1000, null=True, blank=True)
+    paid_subscription = models.BooleanField(default=False)
+    foler_genre = models.ManyToManyField(Genre, related_name='foler_genre')
+    last_uploaded_date = models.DateTimeField(default = timezone.now)
+    tomag_num_total = models.PositiveIntegerField()     #토막글 수 종합
+    heart_num_total = models.PositiveIntegerField()     #공감 수
+    comment_num_total = models.PositiveIntegerField()   #댓글 수 종합
+    views_num_total = models.PositiveIntegerField()
+    writer = models.ForeignKey(TMAuthor)
+
+    def __str__(self):
+        return self.series_title
