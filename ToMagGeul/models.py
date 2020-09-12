19cf30a686c69767429035cc3182a4b8df315298
whitepurple <qazplook@naver.com>
2020-09-12 22:53:16 +0900

    merge

--- ToMagGeul/models.py
+++ ToMagGeul/models.py
@@ -12,10 +12,10 @@ class TMSeries(models.Model):
     paid_subscription = models.BooleanField(default=False)
     series_genre = models.ManyToManyField(Genre, related_name='series_genre')
     last_uploaded_date = models.DateTimeField(default = timezone.now)
-    tomag_num_total = models.PositiveIntegerField()     #토막글 수 종합
-    heart_num_total = models.PositiveIntegerField()     #공감 수
-    comment_num_total = models.PositiveIntegerField()   #댓글 수 종합
-    views_num_total = models.PositiveIntegerField()     #조회 수 종합
+    tomag_num_total = models.PositiveIntegerField(default=0)     #토막글 수 종합
+    heart_num_total = models.PositiveIntegerField(default=0)     #공감 수
+    comment_num_total = models.PositiveIntegerField(default=0)   #댓글 수 종합
+    views_num_total = models.PositiveIntegerField(default=0)     #조회 수 종합
     writer = models.ForeignKey(TMAuthor, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -27,9 +27,9 @@ class TMText(models.Model):
     main_sentence = models.CharField(max_length=200, null=True, blank=True)
     text_content = models.CharField(max_length=5000)
     text_genre =  models.ManyToManyField(Genre, related_name='text_genre')
-    heart_num = models.PositiveIntegerField()     #공감 수
-    comment_num = models.PositiveIntegerField()   #댓글 수
-    views_num = models.PositiveIntegerField()     #조회 수
+    heart_num = models.PositiveIntegerField(default=0)     #공감 수
+    comment_num = models.PositiveIntegerField(default=0)   #댓글 수
+    views_num = models.PositiveIntegerField(default=0)     #조회 수
     date_of_write = models.DateField(default = timezone.now)
     writer = models.ForeignKey(TMAuthor, on_delete=models.CASCADE)
     series = models.ForeignKey(TMSeries, on_delete=models.CASCADE, null=True, blank=True)
